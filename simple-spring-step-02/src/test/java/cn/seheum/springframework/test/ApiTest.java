17ea0e67637e6f5f0408691bca894607cfd02c20
linshixin <linshixin:qwerasdf20221121@gitlab.q-parking.com.cn>
2023-01-12 15:47:42 +0800

    添加测试方法

--- simple-spring-step-02/src/test/java/cn/seheum/springframework/test/ApiTest.java
+++ simple-spring-step-02/src/test/java/cn/seheum/springframework/test/ApiTest.java
@@ -1,4 +1,28 @@
 package cn.seheum.springframework.test;
 
+import cn.seheum.springframework.beans.factory.config.BeanDefinition;
+import cn.seheum.springframework.beans.factory.support.DefaultListableBeanFactory;
+import cn.seheum.springframework.test.bean.UserService;
+import org.junit.Test;
+
 public class ApiTest {
+    @Test
+    public void test_BeanFactory() {
+        //1、初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        //2、注册 bean
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
+        beanFactory.registryBeanDefinition("userService",beanDefinition);
+
+        //3、第一次获取bean
+       UserService userService = (UserService) beanFactory.getBean("userService");
+        System.out.println("userService对象在堆中的地址" + userService);
+       userService.queryUserInfo();
+
+       //4、第二次获取bean
+        UserService userServiceCopy = (UserService) beanFactory.getBean("userService");
+        System.out.println("userServiceCopy对象在堆中的地址" + userServiceCopy);
+        userServiceCopy.queryUserInfo();
+    }
 }
--- simple-spring-step-02/src/test/java/cn/seheum/springframework/test/bean/UserService.java
+++ simple-spring-step-02/src/test/java/cn/seheum/springframework/test/bean/UserService.java
@@ -1,4 +1,8 @@
 package cn.seheum.springframework.test.bean;
 
 public class UserService {
+    public void queryUserInfo(){
+        System.out.println("查询用户信息");
+    }
+
 }

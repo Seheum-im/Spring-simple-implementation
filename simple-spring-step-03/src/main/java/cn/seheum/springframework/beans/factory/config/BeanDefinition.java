3ce69f9b04bb9d0ed887fd3ff0e6787edc6095c1
linshixin <linshixin:qwerasdf20221121@gitlab.q-parking.com.cn>
2023-01-13 10:03:29 +0800

    遗漏的Config

---
+++ simple-spring-step-03/src/main/java/cn/seheum/springframework/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,17 @@
+package cn.seheum.springframework.beans.factory.config;
+
+public class BeanDefinition {
+    private Class beanClass;
+
+    public BeanDefinition(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+}
